16936191bf00b0bdfb9ccfed1f6b02815c9469e9
marcospallone1 <82328624+marcospallone1@users.noreply.github.com>
2022-03-29 16:57:19 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
+window.onload = function(){
+	changeRate();
+}
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var amount = document.getElementById("principal").value;
+	var rateV = document.getElementById("rate").value;
+	var years = Number(document.getElementById("years").value);
+	var currentYear = new Date().getFullYear();
+	var cYN = Number(currentYear)+years;
+	var funRes = (amount * rateV) / 2;
+	
+    document.getElementById("res").innerHTML = "If you deposit <span style='background-color: #FFFF00'>"+amount+"</span><br> at an interest rate of "
+												+"<span style='background-color: #FFFF00'>"+rateV+"</span>, <br>You will receive an amount of "
+												+"<span style='background-color: #FFFF00'>"+funRes+"</span>, <br>in the year <span style='background-color: #FFFF00'>"+cYN+"</span>";
+	
 }
-        
+function changeRate(){
+	var rate = document.getElementById('rate').value;
+	document.getElementById('rangeValue').innerHTML = rate.toString()+'%';
+}
